a20d2468588c857e2b6fc6ca68e9e7c6182db19c
Adam Gilroy <adamgilroy22@gmail.com>
2023-01-23 16:59:01 +0000

    Create calculator class

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,39 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousSumText = document.querySelector('[data-previous-sum]')
-const currentSumText = document.querySelector('[data-current-sum]')
+// Const variables for calculator buttons
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousSumText = document.querySelector('[data-previous-sum]');
+const currentSumText = document.querySelector('[data-current-sum]');
+
+class calculator {
+    constructor(previousSumText, currentSumText) {
+        this.previousSumText = previousSumText;
+        this.currentSumText = currentSumText;
+    };
+
+    clear() {
+
+    };
+
+    delete() {
+
+    };
+
+    appendNumber(number) {
+
+    };
+
+    chooseOperation(operation) {
+
+    };
+
+    calculate() {
+
+    };
+
+    updateDisplay() {
+
+    };
+}
